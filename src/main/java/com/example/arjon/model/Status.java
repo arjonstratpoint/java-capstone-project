b486916e547105b7be34d2bf7bcd226ff9d3ba34
Arjon Layosa <alayosa@stratpoint.com>
2024-05-09 14:02:22 +0800

    Create models

---
+++ src/main/java/com/example/arjon/model/Status.java
@@ -0,0 +1,8 @@
+package com.example.arjon.model;
+
+public enum Status {
+    IDEA,
+    IN_PROGRESS,
+    COMPLETED,
+    PUBLISHED,
+}
---
+++ src/main/java/com/example/arjon/model/Type.java
@@ -0,0 +1,8 @@
+package com.example.arjon.model;
+
+public enum Type {
+    ARTICLE,
+    VIDEO,
+    COURSE,
+    CONFERENCE_TALK
+}
