29fab3742c517cb7db802af35c4349a6c832eff0
Arjon Layosa <alayosa@stratpoint.com>
2024-05-09 13:10:56 +0800

    Initial commit

---
+++ src/main/java/com/example/arjon/ArjonApplication.java
@@ -0,0 +1,13 @@
+package com.example.arjon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArjonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArjonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/arjon/ArjonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arjon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArjonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
