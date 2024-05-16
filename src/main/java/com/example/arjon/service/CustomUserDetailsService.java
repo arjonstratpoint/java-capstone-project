a9a45df4c003b68a70d88daad3b456138d65b150
Arjon Layosa <alayosa@stratpoint.com>
2024-05-16 08:50:26 +0800

    Add initial security config

---
+++ src/main/java/com/example/arjon/service/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.example.arjon.service;
+
+import com.example.arjon.model.Users;
+import com.example.arjon.repository.UserRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Users> optionalUser = userRepository.findByUsername(username);
+        if (optionalUser.isPresent()) {
+            Users user = optionalUser.get();
+            User.UserBuilder builder = User.builder();
+            builder.username(user.username());
+            builder.password(user.password());
+            builder.roles("ADMIN");
+            return builder.build();
+        }else {
+            throw new UsernameNotFoundException("User not found.");
+        }
+    }
+}
